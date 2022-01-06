a1e308d3d64cf67eff2b190413b8f9adb81100b3
Radoslaw Lagan <radoslawlagan@protonmail.com>
2022-01-06 20:53:03 +0100

    Minor fix

--- js/script.js
+++ js/script.js
@@ -3,10 +3,6 @@ const questions = document.querySelectorAll('.faq__question');
 const answers = document.querySelectorAll('.faq__answer');
 const arrows = document.querySelectorAll('.faq__img');
 
-// console.log(faq);
-// console.log(question);
-// console.log(answer);
-
 const roll = (e) => {
 	questions.forEach((question) => question.classList.toggle('active'));
 	answers.forEach((answer) => answer.classList.toggle('hidden'));
