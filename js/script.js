9669c821a78e9fc64f6a543edca64c3e32eb79c8
Radoslaw Lagan <radoslawlagan@protonmail.com>
2022-01-07 14:45:56 +0100

    Fixed JS

--- js/script.js
+++ js/script.js
@@ -3,10 +3,36 @@ const questions = document.querySelectorAll('.faq__question');
 const answers = document.querySelectorAll('.faq__answer');
 const arrows = document.querySelectorAll('.faq__img');
 
-const roll = (e) => {
-	questions.forEach((question) => question.classList.toggle('active'));
-	answers.forEach((answer) => answer.classList.toggle('hidden'));
-	arrows.forEach((arrow) => arrow.classList.toggle('rotate'));
+const roll = (element) => {
+	const question = [...element.children].filter((el) =>
+		el.classList.contains('faq__question')
+	)[0];
+	const answer = [...element.children].filter((el) =>
+		el.classList.contains('faq__answer')
+	)[0];
+	const arrow = [...element.children].filter((el) =>
+		el.classList.contains('faq__img')
+	)[0];
+
+	if (element.classList.contains('faq')) {
+		question.classList.toggle('active');
+		answer.classList.toggle('hidden');
+		arrow.classList.toggle('rotate');
+	}
 };
 
-faqs.forEach((faq) => faq.addEventListener('click', roll));
+faqs.forEach((faq) =>
+	faq.addEventListener('click', function () {
+		roll(this);
+	})
+);
+
+// OLD SOLUTION = OPENS ALL FAQ'S AT SAME TIME //
+
+// const roll = () => {
+// 	questions.forEach((question) => question.classList.toggle('active'));
+// 	answers.forEach((answer) => answer.classList.toggle('hidden'));
+// 	arrows.forEach((arrow) => arrow.classList.toggle('rotate'));
+// };
+
+// faqs.forEach((faq) => faq.addEventListener('click', roll));
